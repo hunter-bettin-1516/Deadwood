59cde8ed490c1f52a5b07e582578b0a209de1bb4
hunterbettin <bettinh@wwu.edu>
2020-02-06 16:28:10 -0800

    added scanner to view

--- View.java
+++ View.java
@@ -1,2 +1,22 @@
 /* View class to be implemented for Model View Controller implementation. For Assgn 2 it will just parse from the command line*/
+import java.util.Scanner;
 
+public class View {
+
+    public static void main(String[] args){
+        Scanner numPlayersScan = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("How many players?");
+
+        String numPlayersStr = numPlayersScan.nextLine();
+
+        int numPlayers = numPlayersStr;
+
+        for (int i = 0; i < numPlayers) {
+
+        }
+    }
+
+
+
+
+}
