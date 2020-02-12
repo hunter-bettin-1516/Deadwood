41d7c0377b9862b0aa14ae609030c03fef120f5f
Dave Bettin <me@davebettin.com>
2020-02-12 02:03:16 -0800

    created a way to randomly assign a different movie to each location and finished populating movie attributes

--- ParseXML.java
+++ ParseXML.java
@@ -21,9 +21,7 @@ public class ParseXML{
         // returns a Document object after loading the book.xml file.
         public Document getDocFromFile(String filename)
         throws ParserConfigurationException{
-        {
-            
-                  
+        {     
            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
            DocumentBuilder db = dbf.newDocumentBuilder();
            Document doc = null;
@@ -39,62 +37,73 @@ public class ParseXML{
         
         } 
         
-        public String getCardName(Document d, int i) {
-         Element root = d.getDocumentElement(); // <cards tag>
-         
-         NodeList cards = root.getElementsByTagName("card");
+         //return card name string
+         public String getCardName(Document d, int i) {
+            Element root = d.getDocumentElement(); // <cards tag>
+            NodeList cards = root.getElementsByTagName("card");
+            Node card = cards.item(i);
+            return card.getAttributes().getNamedItem("name").getNodeValue();
              
-         Node card = cards.item(i);
-         return card.getAttributes().getNamedItem("name").getNodeValue();
-             
-       }
-
-       public ArrayList<Integer> getOnCardRolesArrayList(Document d, int i) {
-         Element root = d.getDocumentElement(); // <board> tag
-         ArrayList<Integer> partsArr = new ArrayList<Integer>();
-            
-         NodeList cards = root.getElementsByTagName("card");
-         
-         Node card = cards.item(i);
-                 
-         NodeList children = card.getChildNodes();
-            
-         for (int j=0; j< children.getLength(); j++){
-               
-            Node sub = children.item(j);
-            
-            if("part".equals(sub.getNodeName())){
-               partsArr.add(Integer.parseInt(sub.getAttributes().getNamedItem("level").getNodeValue())); //populate onCard arrayList                          
-            }
          }
-         return partsArr;
-      }
 
-        public String getSetName(Document d, int i) {
+       //return budget integer
+         public int getBudget(Document d, int i) {
+            Element root = d.getDocumentElement(); //<cards tag>
+            NodeList cards = root.getElementsByTagName("card"); 
+            Node card = cards.item(i);
+            return Integer.parseInt(card.getAttributes().getNamedItem("budget").getNodeValue());  
+         }
+
+         //return on card roles array list
+         public ArrayList<Integer> getOnCardRolesArrayList(Document d, int i) {
+            Element root = d.getDocumentElement(); 
+            ArrayList<Integer> partsArr = new ArrayList<Integer>();
+            NodeList cards = root.getElementsByTagName("card"); 
+            Node card = cards.item(i);         
+            NodeList children = card.getChildNodes();
+            for (int j=0; j< children.getLength(); j++){
+               Node sub = children.item(j);
+               if("part".equals(sub.getNodeName())){
+                  partsArr.add(Integer.parseInt(sub.getAttributes().getNamedItem("level").getNodeValue())); //populate onCard arrayList                          
+               }
+            }
+            return partsArr;
+         }
+
+         //return on card part names array list
+         public ArrayList<String> getPartNamesArrayList(Document d, int i) {
             Element root = d.getDocumentElement(); // <board> tag
-            
+            ArrayList<String> partsArr = new ArrayList<String>();
+            NodeList cards = root.getElementsByTagName("card"); 
+            Node card = cards.item(i);         
+            NodeList children = card.getChildNodes();
+            for (int j=0; j< children.getLength(); j++){
+               Node sub = children.item(j);
+               if("part".equals(sub.getNodeName())){
+                  partsArr.add(sub.getAttributes().getNamedItem("name").getNodeValue()); //populate on card part names arrayList                          
+               }
+            }
+            return partsArr;
+         }
+
+         public String getSetName(Document d, int i) {
+            Element root = d.getDocumentElement(); // <board> tag
             NodeList sets = root.getElementsByTagName("set");
-                
             Node set = sets.item(i);
             return set.getAttributes().getNamedItem("name").getNodeValue();
-                
         }
 
 
-
-        public ArrayList<String> getNeighborArrayList(Document d, int i) {
+        //return neighbor array list
+         public ArrayList<String> getNeighborArrayList(Document d, int i) {
             Element root = d.getDocumentElement(); // <board> tag
             ArrayList<String> neighborArr = new ArrayList<String>();
-               
             NodeList sets = root.getElementsByTagName("set");
             //reads data from the nodes
             Node set = sets.item(i);
-               
             //reads data         
             NodeList children = set.getChildNodes();
-               
-            for (int j=0; j< children.getLength(); j++){
-                  
+            for (int j=0; j< children.getLength(); j++){ 
                Node sub = children.item(j);
                
                if("neighbors".equals(sub.getNodeName())){
@@ -113,25 +122,20 @@ public class ParseXML{
          //return shotCounters array list
          public ArrayList<String> getShotCountersArrayList(Document d, int i) {
             Element root = d.getDocumentElement(); // <board> tag
-            ArrayList<String> takesArr = new ArrayList<String>();
-               
+            ArrayList<String> takesArr = new ArrayList<String>();  
             NodeList sets = root.getElementsByTagName("set");
             //reads data from the nodes
             Node set = sets.item(i);
-               
             //reads data         
             NodeList children = set.getChildNodes();
-               
             for (int j=0; j< children.getLength(); j++){
-                  
                Node sub = children.item(j);
-               
                if("takes".equals(sub.getNodeName())){
                   NodeList grandchildren = sub.getChildNodes();
                   for (int z=0; z< grandchildren.getLength(); z++) {
                      Node take = grandchildren.item(z);
                      if("take".equals(take.getNodeName())) {  
-                        takesArr.add(take.getAttributes().getNamedItem("number").getNodeValue()); //populate neighbor arrayList                        
+                        takesArr.add(take.getAttributes().getNamedItem("number").getNodeValue()); //populate shotcounter arrayList                        
                      }
                   }   
                }
@@ -139,108 +143,25 @@ public class ParseXML{
             return takesArr;
          }
 
-         //return parts array list
+         //return offcardroles array list
          public ArrayList<Integer> getOffCardRolesArrayList(Document d, int i) {
             Element root = d.getDocumentElement(); // <board> tag
-            ArrayList<Integer> partsArr = new ArrayList<Integer>();
-               
+            ArrayList<Integer> partsArr = new ArrayList<Integer>(); 
             NodeList sets = root.getElementsByTagName("set");
-            
-            Node set = sets.item(i);
-                    
-            NodeList children = set.getChildNodes();
-               
-            for (int j=0; j< children.getLength(); j++){
-                  
+            Node set = sets.item(i);      
+            NodeList children = set.getChildNodes(); 
+            for (int j=0; j< children.getLength(); j++){   
                Node sub = children.item(j);
-               
                if("parts".equals(sub.getNodeName())){
                   NodeList grandchildren = sub.getChildNodes();
                   for (int z=0; z< grandchildren.getLength(); z++) {
                      Node part = grandchildren.item(z);
                      if("part".equals(part.getNodeName())) {  
-                        partsArr.add(Integer.parseInt(part.getAttributes().getNamedItem("level").getNodeValue())); //populate neighbor arrayList                        
+                        partsArr.add(Integer.parseInt(part.getAttributes().getNamedItem("level").getNodeValue())); //populate offcardroles arrayList                        
                      }
                   }   
                }
             }
             return partsArr;
          }
-
-
-
-
-
-        // reads data from XML file and prints data
-        public static void readBoardData(Document d){
-        
-            Element root = d.getDocumentElement(); // <board> tag
-            
-            NodeList sets = root.getElementsByTagName("set");
-            System.out.println(root.toString());
-            System.out.println(sets.toString());
-            for (int i=0; i<sets.getLength();i++){
-                
-                System.out.println("Printing information for sets "+(i+1));
-                
-                //reads data from the nodes
-                Node set = sets.item(i);
-                String setNeighbors = set.getAttributes().getNamedItem("name").getNodeValue();
-                System.out.println("current set = "+setNeighbors);
-                
-                //reads data
-                                             
-                NodeList children = set.getChildNodes();
-                
-                for (int j=0; j< children.getLength(); j++){
-                    
-                  Node sub = children.item(j);
-                
-                  if("neighbors".equals(sub.getNodeName())){
-                     NodeList grandchildren = sub.getChildNodes();
-                     for (int z=0; z< grandchildren.getLength(); z++) {
-                        Node neighbor = grandchildren.item(z);
-                        if("neighbor".equals(neighbor.getNodeName())) {  
-                           String neighborList = neighbor.getAttributes().getNamedItem("name").getNodeValue();
-                           System.out.println("Neighbors = "+neighborList);
-                        }
-                     }   
-                  }
-                  
-                  else if("parts".equals(sub.getNodeName())){
-                     NodeList grandchildren = sub.getChildNodes();
-                     for (int z=0; z< grandchildren.getLength(); z++) {
-                        Node part = grandchildren.item(z);
-                        if("part".equals(part.getNodeName())) {  
-                           String partNameList = part.getAttributes().getNamedItem("name").getNodeValue();
-                           System.out.println("part name = "+partNameList);
-                           String partList = part.getAttributes().getNamedItem("level").getNodeValue();
-                           System.out.println("part level = "+partList);
-                        }
-                     }   
-                  }
-                  else if("takes".equals(sub.getNodeName())){
-                     NodeList grandchildren = sub.getChildNodes();
-                     for (int z=0; z< grandchildren.getLength(); z++) {
-                        Node take = grandchildren.item(z);
-                        if("take".equals(take.getNodeName())) {  
-                           String takesList = take.getAttributes().getNamedItem("number").getNodeValue();
-                           System.out.println("Take number = "+takesList);
-                        }
-                     }   
-                  }
-                                 
-                
-                } //for childnodes
-                
-                System.out.println("\n");
-                
-            }//for book nodes
-        
-        }// method
-    
-    
-
-
-
 }//class
