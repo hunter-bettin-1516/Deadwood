db016218b823ca1e1851e40cc2c81d622596ac61
hunterbettin <bettinh@wwu.edu>
2020-02-21 13:54:11 -0800

    Text based game is complete

--- View.java
+++ View.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 import java.util.*;
 
 public class View {
-    
     public static void main(String[] args) throws Exception {
         Moderator mod = new Moderator();
         Scanner scan = new Scanner(System.in);
@@ -27,55 +26,32 @@ public class View {
                 if (mod.getIsPlayerWorking(i) == true) {
                     //currently working stuff
                     System.out.println("This is the movie: " + mod.getMovieTitle(i) + "'s budget: $" + mod.getMovieBudget(i));
-                    System.out.println("Would you like to 'act' or 'rehearse'?");
+                    System.out.println("\nWould you like to 'act' or 'rehearse'?");
                     String actOrRehearse = scan.nextLine();
                     mod.work(actOrRehearse, i);
-
                 } else {
                     System.out.println("What is " + mod.getPlayerName(i) + "'s decision? ('move', 'take a role', 'upgrade')");
-                    //repear take a role code here
+                    //repeat take a role code here
                     String decision = scan.nextLine();
                     if (decision.equals("move") || decision.equals("Move")) {
                         System.out.println('\n' + "You can move to these locations: " + mod.getPlayersNeighbors(i) + ". Where would you like to go?");
                         String newLocation = scan.nextLine();
                         mod.move(newLocation, i); //update player location
-                        System.out.println('\n' + "You have moved to " + newLocation + ", filming: " + mod.getMovieTitle(i) + " with a budget of $" + mod.getMovieBudget(i) + '\n');
-                        System.out.println("Here is the list of main roles followed by the list of their required ranks: " + mod.getOnCardRoles(i) +", " + mod.getOnCardRanks(i) + '\n');
-                        System.out.println("Here is the list of supporting roles followed by the list of their required ranks: " + mod.getOffCardRoles(i) + ", " + mod.getOffCardRanks(i) + '\n');
-                        System.out.println("Would you like to 'take a role' or 'stay' on location: " + newLocation + "? (Your rank must be >= the role rank.)");
-                        String takeRoleOrStay = scan.nextLine();
-                        if (takeRoleOrStay.equals("take a role")) {
-                            //while loop for if user chooses a role that requires a higher rank than what they have
-                            String onOrOff = "";
-                            String role = "";
-                            boolean validRole = false;
-                            int j = 0;
-                            while (validRole == false) {
-                                if (j == 0) {
-                                    System.out.println("Would you like to take a 'main role' or 'supporting role?' ");
-                                    onOrOff = scan.nextLine();
-                                    System.out.println("What is the name of the role that you would like to take?");   
-                                } else {
-                                    System.out.println("You do not have the required Rank to take that role.");
-                                    System.out.println("Would you like to take a 'main role' or 'supporting role?' ");
-                                    onOrOff = scan.nextLine();
-                                    System.out.println("What is the name of the role that you would like to take?");  
-                                }
-                                role = scan.nextLine();
-                                validRole = mod.verifyRole(onOrOff, role, i);
-                                j++;
-                            }
-                            mod.setPlayerRole(onOrOff, role, i);
-                            System.out.println("Turn Over");
-                        } else if(decision.equals("upgrade")) {
-                            if (true) { /*currentlocation == casting office*/
-                                System.out.println("RANK   DOLLARS   CREDITS \n----   -------   ------- \n2   4   5 \n3   10   10 \n4   18   15 \n5   28   20 \n6   40   25");
-                                System.out.println("\nWhat rank would you like to upgrade to?");
-                                String toRank = scan.nextLine();
-                                System.out.println("\nWould you like to upgrade your rank using 'dollars' or 'credits'");
-                                String dollarsOrCredits = scan.nextLine();
-
-                                mod.upgradeAtOffice(toRank, dollarsOrCredits, i);  
+                        if (mod.getPlayerLocation(i).equals("office")) {
+                            System.out.println("\nYou have moved to the Casting Office");
+                            System.out.println("\nWould you like to 'upgrade or 'stay on location: office?");
+                        } else {
+                            System.out.println('\n' + "You have moved to " + newLocation + ", filming: " + mod.getMovieTitle(i) + " with a budget of $" + mod.getMovieBudget(i) + '\n');
+                            System.out.println("Here is the list of main roles followed by the list of their required ranks: " + mod.getOnCardRoles(i) +", " + mod.getOnCardRanks(i) + '\n');
+                            System.out.println("Here is the list of supporting roles followed by the list of their required ranks: " + mod.getOffCardRoles(i) + ", " + mod.getOffCardRanks(i) + '\n');
+                            System.out.println("Would you like to 'take a role' or 'stay' on location: " + newLocation + "? (Your rank must be >= the role rank.)");
+                        }
+                        String takeRoleUpgradeOrStay = scan.nextLine();
+                        if (takeRoleUpgradeOrStay.equals("take a role")) {
+                            takeARole(mod, scan, i);
+                        } else if(takeRoleUpgradeOrStay.equals("upgrade")) {
+                            if (mod.getPlayerLocation(i).equals("office")) {
+                                upgrade(mod, scan, i);
                             }
                         } else {
                             System.out.println("Turn Over");
@@ -84,48 +60,78 @@ public class View {
                             System.out.println("Here is the list of main roles followed by the list of their required ranks: " + mod.getOnCardRoles(i) +", " + mod.getOnCardRanks(i) + '\n');
                             System.out.println("Here is the list of supporting roles followed by the list of their required ranks: " + mod.getOffCardRoles(i) + ", " + mod.getOffCardRanks(i) + '\n');
                             System.out.println("Would you like to 'take a role' or 'stay'? (Your rank must be >= the role rank.)");
-                            String onOrOff = "";
-                            String role = "";
-                            boolean validRole = false;
-                            int j = 0;
-                            while (validRole == false) {
-                                if (j == 0) {
-                                    System.out.println("Would you like to take a 'main role' or 'supporting role?' ");
-                                    onOrOff = scan.nextLine();
-                                    System.out.println("What is the name of the role that you would like to take?");   
-                                } else {
-                                    System.out.println("You do not have the required Rank to take that role.");
-                                    System.out.println("Would you like to take a 'main role' or 'supporting role?' ");
-                                    onOrOff = scan.nextLine();
-                                    System.out.println("What is the name of the role that you would like to take?");  
-                                }
-                                role = scan.nextLine();
-                                validRole = mod.verifyRole(onOrOff, role, i);
-                                j++;
-                            }
-                            mod.setPlayerRole(onOrOff, role, i);
-                            System.out.println("Turn Over");
+                            takeARole(mod, scan, i);
                         } else if (decision.equals("upgrade")) {
-                            if (true) { /*currentlocation == casting office*/
-                                System.out.println("RANK   DOLLARS   CREDITS \n----   -------   ------- \n2   4   5 \n3   10   10 \n4   18   15 \n5   28   20 \n6   40   25");
-                                System.out.println("\nWhat rank would you like to upgrade to?");
-                                String toRank = scan.nextLine();
-                                System.out.println("\nWould you like to upgrade your rank using 'dollars' or 'credits'");
-                                String dollarsOrCredits = scan.nextLine();
-
-                                mod.upgradeAtOffice(toRank, dollarsOrCredits, i);
+                            if (mod.getPlayerLocation(i).equals("office")) {
+                                upgrade(mod, scan, i);
                             }
                         } else {
-                            System.out.println("Turn Over");
+                            System.out.println("\nTurn Over");
                         }
                     }
-                    
+                }   
+            }
+            scan.close();  
+    }   
+
+    public static void takeARole(Moderator mod, Scanner scan, int i) {
+        //while loop for if user chooses a role that requires a higher rank than what they have
+        String onOrOff = "";
+        String role = "";
+        boolean validRole = false;
+        int j = 0;
+        while (validRole == false) {
+            if (j == 0) {
+                System.out.println("\nWould you like to take a 'main role' or 'supporting role?' ");
+                onOrOff = scan.nextLine();
+                if (onOrOff.equals("main role")) {
+                    System.out.println("\nWhat is the name of the role that you would like to take? " + mod.getOnCardRoles(i));  
+                } else if (onOrOff.equals("supporting role")) {
+                    System.out.println("\nWhat is the name of the role that you would like to take? " + mod.getOffCardRoles(i)); 
+                }  
+            } else {
+                System.out.println("\nYou do not have the required Rank to take that role.");
+                System.out.println("\nWould you like to take a 'main role' or 'supporting role?' ");
+                onOrOff = scan.nextLine();
+                if (onOrOff.equals("main role")) {
+                    System.out.println("\nWhat is the name of the role that you would like to take? " + mod.getOnCardRoles(i));  
+                } else if (onOrOff.equals("supporting role")) {
+                    System.out.println("\nWhat is the name of the role that you would like to take? " + mod.getOffCardRoles(i)); 
                 }
             }
-
-        //print winner
-        scan.close();
+            role = scan.nextLine();
+            validRole = mod.verifyRole(onOrOff, role, i);
+            j++;
         }
+        mod.setPlayerRole(onOrOff, role, i);
+        System.out.println("\nTurn Over");
+    }
+
+    public static void upgrade(Moderator mod, Scanner scan, int i) {
+            boolean upgradeLoop = true;
+            while (upgradeLoop == true) {
+                System.out.println("\nRANK   DOLLARS   CREDITS \n----   -------   ------- \n2      4         5 \n3      10        10 \n4      18        15 \n5      28        20 \n6      40        25");
+                System.out.println("\nWhat rank would you like to upgrade to?");
+                String toRank = scan.nextLine();
+                System.out.println("\nWould you like to upgrade your rank using 'dollars' or 'credits'");
+                String dollarsOrCredits = scan.nextLine();
+
+                if(mod.canPlayerUpgrade(toRank, dollarsOrCredits, i) == true) { //check if player has enough dollars or credits to rankup
+                    mod.upgradeAtOffice(toRank, dollarsOrCredits, i); 
+                    upgradeLoop = false;
+                } else {
+                    System.out.println("\nYou do not have enough "+ dollarsOrCredits + " to upgrade to rank: " + toRank);
+                    System.out.println("\nWould you like to ranking up with another form of payment? ('yes' or 'no')");
+                    String otherPayment = scan.nextLine();
+                    if (otherPayment.equals("yes")) {
+                        upgradeLoop = true;
+                    } else if (otherPayment.equals("no")){
+                        upgradeLoop = false;
+                    } 
+                }
+            }
+            System.out.println("\nTurn Over");
+    }
 }
 
 
