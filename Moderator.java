90358fb6a12503c093e4878225b3f27b04d3ce7c
hunterbettin <bettinh@wwu.edu>
2020-02-21 17:05:14 -0800

    final touch ups and comments. Ready for turn in

--- Moderator.java
+++ Moderator.java
@@ -16,7 +16,7 @@ public class Moderator {
     private int dayCount = 1;
     private int maxDays = 4;
     private boolean gameOver = false;
-    private int activeMovies = 10; //comparison for new day... decrement when locations[i].movie.isAWrap == true;
+    private int activeMovies = 10; 
     private ParseXML xml = new ParseXML();
     private Location[] locations = new Location[12];
     private Movie[] movies = new Movie [40];
@@ -28,6 +28,7 @@ public class Moderator {
     private CastingOffice castOffice = new CastingOffice();
     private int winner = -1;
     
+    //getters and playerCount setter so class View can access/print certain data
     public String getPlayerLocation(int i) {
         return this.players[i].getLocation();
     }
@@ -141,6 +142,7 @@ public class Moderator {
         }
     }
 
+    //init player instances
     public void inizializePlayers(String ID, int i) {
         
         this.players[i] = new Player();
@@ -148,7 +150,7 @@ public class Moderator {
         this.players[i].setPlayerID(ID);
 
         if (this.playerCount < 4) {
-            this.maxDays = 3;
+            this.maxDays = 3; 
         } 
         else if (this.playerCount == 5) {
             this.players[i].setCredits(2);
@@ -161,6 +163,7 @@ public class Moderator {
         }
     }
 
+    //castingOffice class - method call for upgrading Rank
     public void upgradeAtOffice(String toRank, String dollarsOrCredits, int i) {
         this.castOffice.upgradeRank(toRank, dollarsOrCredits, i, players);
         System.out.println("\nCongrats! You are now rank: " + this.players[i].getPlayerRank() + ". Your remaing dollars are: " + this.players[i].getDollars() +". Your remaing credits are: " + this.players[i].getCredits() + ".");
@@ -184,11 +187,13 @@ public class Moderator {
         return false;
     }
     
+    //displays user stats at start of every turn
     public String displayPlayerStats(int i) {
         String stats = "\nIt is " + players[i].getPlayerID() + "'s turn. ||| Current Location = " + players[i].getLocation() + " ||| Current role = " + players[i].getRole() + " ||| Rehearsal chips = " + players[i].getRehearsalCount() + " ||| Rank = " + players[i].getPlayerRank() + " ||| Dollars = " + players[i].getDollars() + " ||| Credits = " + players[i].getCredits(); 
         return stats;
     }
 
+    //sets main/supporting roles to specified player[i]
     public void setPlayerRole(String onOrOff, String role, int i) {
         if(onOrOff.equals("main role")){
             this.locationMap.get(this.players[i].getLocation()).getLocationsMovieCard().addOnCardWorker(i);
@@ -206,6 +211,7 @@ public class Moderator {
         this.players[i].setWorking(true);
     }
 
+    //boolean verifies if player[i] has required rank for a given role 
     public boolean verifyRole(String onOrOff, String role, int i){
         if (onOrOff.equals("main role")) {
             int roleIndex = this.locationMap.get(this.players[i].getLocation()).getLocationsMovieCard().getPartNameList().indexOf(role);
@@ -223,8 +229,8 @@ public class Moderator {
         return false;
     } 
 
+    //act/rehearse function for specified player[i]
     public void work(String decision, int i) {
-        //act or rehearse for a specific player
         int movieBudget = locationMap.get(players[i].getLocation()).getLocationsMovieCard().getMovieBudget();
         int roll = 0;
         if (decision.equals("act")) {
@@ -234,7 +240,7 @@ public class Moderator {
                 this.SuccessfulAct(i);
             } else {
                 System.out.println("You rolled a: " + roll + ". With a rehearsal count of: " + this.players[i].getRehearsalCount() + ". Act Unsuccessful. Sucks.");
-                offCardFailedRoll(i);
+                failedRoll(i);
                 System.out.println("Turn Over.");
             }
         } else if (decision.equals("rehearse")) {
@@ -250,6 +256,7 @@ public class Moderator {
         }
     }
 
+    //decrements shot counts, decrements activeMovies, calls payoutPackage class for payout calculations, calls newDay when only 1 movie is left
     public void SuccessfulAct(int i) {
         this.locationMap.get(this.players[i].getLocation()).removeShotCounter(); // remove shotcounter
         System.out.println("\nThese are the remaining shotcounters: " + this.locationMap.get(this.players[i].getLocation()).getShotCounters());
@@ -257,30 +264,30 @@ public class Moderator {
         if (this.locationMap.get(this.players[i].getLocation()).getShotCounters() == 0) {
             this.locationMap.get(this.players[i].getLocation()).getLocationsMovieCard().setMovieIsAWrap(true);
             System.out.println("\n---THE MOVIE IS A WRAP---");
-            this.activeMovies--; 
+            this.activeMovies = this.activeMovies - 1;
             System.out.println("There are: " + this.activeMovies + " remaining movies.");
-            //if only one movie card left, start new day
-            if (this.activeMovies >= 1) { //pay players
-                //check if people are working on card
+            if (this.activeMovies >= 1) { 
                 ArrayList<Integer> onCardWorkerList = this.locationMap.get(this.players[i].getLocation()).getLocationsMovieCard().getOnCardWorkers();
                 ArrayList<Integer> offCardWorkerList = this.locationMap.get(this.players[i].getLocation()).getOffCardWorkers();
+                //check if people are working on card
                 if (onCardWorkerList.size() > 0) {
-                    //this.payPackage.onCardPayout(onCardWorkerList);
-                    //set all onCard workers to workingFlag = false.
                     this.payPackage.onCardPayout(onCardWorkerList, this.locationMap, this.players);
                     if (offCardWorkerList.size() > 0) {
                         this.payPackage.offCardPayout(offCardWorkerList, this.locationMap, this.players);
                     }
                 }
                 resetPlayerWorkingStats(onCardWorkerList, offCardWorkerList); 
-            } else if (this.activeMovies == 1) {
+            }
+            //if only one movie card left, start new day
+            if (this.activeMovies == 1) {
                 try {
                     this.newDay();
                 } catch (Exception e) {
+                    e.printStackTrace();
                     System.out.println("newDay() is bugged");
                 }
             } 
-            
+        //else payments for successful act when movie is NOT a wrap (shoutCounters > 0)
         } else {
             if (this.players[i].getOnCard() == true) {
                 this.players[i].addCredits(2);
@@ -291,7 +298,8 @@ public class Moderator {
         }
     }
 
-    public void offCardFailedRoll(int i) {
+    //payments for failed rolls
+    public void failedRoll(int i) {
         if (players[i].getOnCard() == false) {
             this.players[i].addDollars(1);
             System.out.println("Thanks for nothing! Here's a dollar.");
@@ -300,6 +308,7 @@ public class Moderator {
         }
     }
 
+    //reset: player role, rehearsal chips and working flag when movie is a Wrap
     public void resetPlayerWorkingStats(ArrayList<Integer> onCardWorkers, ArrayList<Integer> offCardWorkers) {
         for (int i = 0; i < onCardWorkers.size(); i++) {
             int playerIndex = onCardWorkers.get(i);
@@ -315,28 +324,24 @@ public class Moderator {
         }
     }
     
-
+    //update player[i]s Location when they choose to move
     public void move(String location, int i) {
-        //update Location 
         players[i].setLocation(location);
     }
 
-
-    public void upgrade(int playerID) {
-        //check location for casting office
-    }
-
+    //increments day counter, puts players back to 'trailer', reshuffles movie cards and shotcounters
     public void newDay() throws Exception {
         Document doc = xml.getDocFromFile("board.xml");
         this.dayCount = this.dayCount + 1;
-        if (this.dayCount == this.maxDays) {
+        //call endGame when the last day is over
+        if (this.dayCount > this.maxDays) {
             this.endGame();
         } else {
-            //reset board put players back to trailer
+            System.out.println("\nIt is now day: " + this.dayCount + ". Everyone is moved back to the Trailer");
             for (int i = 0; i < playerCount; i++) {
                 this.players[i].setLocation("Trailer");
+                this.players[i].setWorking(false);
             }
-            //reshuffle new movie cards to locations
             for (int i = 0; i < 10; i++) {
                 this.locations[i].setShotCounters(xml.getShotCountersArrayList(doc, i));
                 boolean findNewCard = true;
@@ -355,14 +360,12 @@ public class Moderator {
 
     //calculate winner and end game.
     public void endGame() {
-        for (int i = 0; i < this.players.length; i++) {
+        for (int i = 0; i < this.playerCount; i++) {
             int rankScore = (this.players[i].getPlayerRank()) * 5;
             int credits = this.players[i].getCredits();
             int dollars = this.players[i].getDollars();
-
             int score = rankScore + credits + dollars;
             this.finalScores[i] = score;
-            
         }
         int bestScore = 0;
         for (int i = 0; i < this.players.length; i++) {
@@ -374,6 +377,9 @@ public class Moderator {
         this.gameOver = true;
         System.out.println("\n### GAME-OVER ### \n THE WINNER IS: " + this.players[this.winner].getPlayerID() + ". With a score of: " + bestScore);
     }
+
+
+    /*----------------------------------------------------------------TEST FUNCTIONS------------------------------------------------------------------------*/
 
     public void testLocations() {
         //loop for testing data is being stored properly within location
@@ -422,6 +428,4 @@ public class Moderator {
             System.out.println("");
         }
     }
-
-
 }
